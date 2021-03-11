95dc6092d03c11a97928fb47db4685def414a8ce
Davide Pizzato <davidepizzato@webmapp.it>
2021-03-11 14:14:47 +0100

    Added e2e tests for lists

---
+++ cypress/integration/03_lists/01_provinces/00_check_values.spec.js
@@ -0,0 +1,64 @@
+describe('Provinces list', () => {
+    beforeEach(() => {
+        cy.visit('/nova/login');
+        cy.get('input[name=email]').type('team@webmapp.it');
+        cy.get('input[name=password]').type('webmapp');
+        cy.contains('Login').click();
+        cy.contains('Province').click();
+    });
+
+    afterEach(() => {
+        cy.get('.v-popover.dropdown-right button.rounded').click();
+        cy.contains('Logout').click();
+    });
+
+    it('should show a table with name, code, full_code, region, areas number, sectors number', () => {
+        const tableSelector = 'table[data-testid=resource-table]',
+            tableHeadSelector = tableSelector + ' > thead',
+            tableBodySelector = tableSelector + ' > tbody';
+        cy.get('h1').contains('Province').should('be.visible');
+        cy.get(tableSelector).should('be.visible');
+        cy.get(tableHeadSelector)
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('name', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('code', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('full code', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('region', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('areas', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('sectors', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableBodySelector + ' > tr').each((tr) => {
+            cy.wrap(tr).children('td').each((td) => {
+                expect(td).to.be.visible;
+                expect(td.text()).to.match(/.+/);
+            });
+        });
+    });
+
+    it('should show the download geojson/shape for all the rows', () => {
+        const tableSelector = 'table[data-testid=resource-table]',
+            tableBodySelector = tableSelector + ' > tbody';
+        cy.get(tableBodySelector + ' > tr > td:last-child').each((td, index) => {
+            cy.wrap(td)
+                .contains('actions', {matchCase: false})
+                .should('be.visible')
+            cy.wrap(td).click();
+            cy.contains('Download Geojson', {matchCase: false})
+                .should('be.visible');
+            cy.contains('Download Shape', {matchCase: false})
+                .should('be.visible');
+            cy.wrap(td).click();
+        });
+    });
+})
---
+++ cypress/integration/03_lists/02_areas/00_check_values.spec.js
@@ -0,0 +1,64 @@
+describe('Areas list', () => {
+    beforeEach(() => {
+        cy.visit('/nova/login');
+        cy.get('input[name=email]').type('team@webmapp.it');
+        cy.get('input[name=password]').type('webmapp');
+        cy.contains('Login').click();
+        cy.contains('Aree').click();
+    });
+
+    afterEach(() => {
+        cy.get('.v-popover.dropdown-right button.rounded').click();
+        cy.contains('Logout').click();
+    });
+
+    it('should show a table with name, code, full_code, region, province, sectors number', () => {
+        const tableSelector = 'table[data-testid=resource-table]',
+            tableHeadSelector = tableSelector + ' > thead',
+            tableBodySelector = tableSelector + ' > tbody';
+        cy.get('h1').contains('Aree').should('be.visible');
+        cy.get(tableSelector).should('be.visible');
+        cy.get(tableHeadSelector)
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('name', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('code', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('full code', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('region', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('province', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableHeadSelector)
+            .contains('sectors', {matchCase: false})
+            .should('be.visible');
+        cy.get(tableBodySelector + ' > tr').each((tr) => {
+            cy.wrap(tr).children('td').each((td) => {
+                expect(td).to.be.visible;
+                expect(td.text()).to.match(/.+/);
+            });
+        });
+    });
+
+    it('should show the download geojson/shape for all the rows', () => {
+        const tableSelector = 'table[data-testid=resource-table]',
+            tableBodySelector = tableSelector + ' > tbody';
+        cy.get(tableBodySelector + ' > tr > td:last-child').each((td, index) => {
+            cy.wrap(td)
+                .contains('actions', {matchCase: false})
+                .should('be.visible')
+            cy.wrap(td).click();
+            cy.contains('Download Geojson', {matchCase: false})
+                .should('be.visible');
+            cy.contains('Download Shape', {matchCase: false})
+                .should('be.visible');
+            cy.wrap(td).click();
+        });
+    });
+})
