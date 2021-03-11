cd1cb0e45e62cc7ebb9a6e0ff45c5c5b16e44fc5
Davide Pizzato <davidepizzato@webmapp.it>
2021-03-11 12:48:54 +0100

    Added first unit tests

--- database/factories/ProvinceFactory.php
+++ database/factories/ProvinceFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Province;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use MStaack\LaravelPostgis\Geometries\Point;
 
 class ProvinceFactory extends Factory
 {
@@ -14,6 +15,8 @@ class ProvinceFactory extends Factory
      */
     protected $model = Province::class;
 
+    private static $id = 1;
+
     /**
      * Define the model's default state.
      *
@@ -21,8 +24,15 @@ class ProvinceFactory extends Factory
      */
     public function definition()
     {
+        $id = self::$id;
+        $code = chr($id) . chr($id);
+        self::$id = self::$id + 1;
         return [
-            //
+            'id' => $id,
+            'name' => $this->faker->name,
+            'geometry' => (new Point($this->faker->latitude, $this->faker->longitude))->toWKT(),
+            'code' => $code,
+            'full_code' => chr($id) . $code
         ];
     }
 }
