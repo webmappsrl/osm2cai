f4adbc41a718344d4cda1f5ae32da1a071b63ef5
Davide Pizzato <davidepizzato@webmapp.it>
2021-04-21 12:55:38 +0200

    Small footer update

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
-    Osm2Cai v{{ config('app.version') }}
+    {{ config('app.name') }} v{{ config('app.version') }}
     <span class="px-1">&middot;</span>
     &copy; <a href="https://webmapp.it" class="text-primary dim no-underline">Webmapp</a>
     <span class="px-1">&middot;</span>
