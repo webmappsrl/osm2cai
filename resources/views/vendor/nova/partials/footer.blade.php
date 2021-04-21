bac3d843053ea5b050e21a2a5031835b94e0ed91
Davide Pizzato <davidepizzato@webmapp.it>
2021-04-21 12:54:49 +0200

    Updated version, added versions information in the footer

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -2,4 +2,13 @@
     Osm2Cai v{{ config('app.version') }}
     <span class="px-1">&middot;</span>
     &copy; <a href="https://webmapp.it" class="text-primary dim no-underline">Webmapp</a>
+    <span class="px-1">&middot;</span>
+    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    v{{ \Laravel\Nova\Nova::version() }}
+    <span class="px-1">&middot;</span>
+    <a href="https://laravel.com/" class="text-primary dim no-underline">Laravel</a>
+    v{{ app()->version() }}
+    <span class="px-1">&middot;</span>
+    <a href="https://php.net/" class="text-primary dim no-underline">PHP</a>
+    v{{ phpversion() }}
 </p>
