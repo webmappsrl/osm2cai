8112ef1dd6138182f51a4142f44ed5da1176955b
Alessio Piccioli <piccioli@netseven.it>
2021-05-21 22:35:56 +0200

    Problem (not solved) with test

--- tests/Feature/ShapefileGenerationTest.php
+++ tests/Feature/ShapefileGenerationTest.php
@@ -8,37 +8,28 @@ use App\Models\Region;
 use App\Models\Sector;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
-class ShapefileGenerationTest extends TestCase {
+class ShapefileGenerationTest extends TestCase
+{
     use RefreshDatabase;
 
-    public function testRegionShapefileGeneration() {
+    public function testRegionShapefileGeneration()
+    {
         Storage::disk('public')->deleteDirectory('shape_files');
-        $region = Region::factory()->has(
-            Province::factory()->has(
-                Area::factory()->has(
-                    Sector::factory()->count(2)
-                )->count(2)
-            )->count(2)
-        )->create()->first();
-
+        $region = $this->_getRegion();
         $shapefileLocation = $region->getShapefile();
 
         $this->assertTrue(Storage::disk('public')->exists($shapefileLocation));
     }
 
-    public function testProvinceShapefileGeneration() {
+    public function testProvinceShapefileGeneration()
+    {
         Storage::disk('public')->deleteDirectory('shape_files');
-        Region::factory()->has(
-            Province::factory()->has(
-                Area::factory()->has(
-                    Sector::factory()->count(2)
-                )->count(2)
-            )->count(1)
-        )->create()->first();
+        $region = $this->_getRegion();
 
         $province = Province::first();
         $shapefileLocation = $province->getShapefile();
@@ -46,15 +37,10 @@ class ShapefileGenerationTest extends TestCase {
         $this->assertTrue(Storage::disk('public')->exists($shapefileLocation));
     }
 
-    public function testAreaShapefileGeneration() {
+    public function testAreaShapefileGeneration()
+    {
         Storage::disk('public')->deleteDirectory('shape_files');
-        Region::factory()->has(
-            Province::factory()->has(
-                Area::factory()->has(
-                    Sector::factory()->count(2)
-                )->count(1)
-            )->count(1)
-        )->create()->first();
+        $region = $this->_getRegion();
 
         $area = Area::first();
         $shapefileLocation = $area->getShapefile();
@@ -62,19 +48,38 @@ class ShapefileGenerationTest extends TestCase {
         $this->assertTrue(Storage::disk('public')->exists($shapefileLocation));
     }
 
-    public function testSectorShapefileGeneration() {
+    public function testSectorShapefileGeneration()
+    {
         Storage::disk('public')->deleteDirectory('shape_files');
-        Region::factory()->has(
-            Province::factory()->has(
-                Area::factory()->has(
-                    Sector::factory()->count(1)
-                )->count(1)
-            )->count(1)
-        )->create()->first();
+        $region = $this->_getRegion();
 
         $sector = Sector::first();
         $shapefileLocation = $sector->getShapefile();
 
         $this->assertTrue(Storage::disk('public')->exists($shapefileLocation));
     }
+
+    private function _getGeometry($type, $coordinates): string
+    {
+        $geojson = json_encode([
+            "type" => $type,
+            "coordinates" => $coordinates
+        ]);
+        $res = DB::select(DB::raw('SELECT ST_GeomFromGeoJSON(\'' . $geojson . '\') as geom'));
+        return $res[0]->geom;
+    }
+
+    private function _getRegion(): Region
+    {
+        $fake_geom = ['geometry' => $this->_getGeometry('Polygon', [[[-2, -2], [-2, -1], [-1, -1], [-2, -2]]])];
+        $region = Region::factory($fake_geom)->has(
+            Province::factory($fake_geom)->has(
+                Area::factory($fake_geom)->has(
+                    Sector::factory($fake_geom)->count(2)
+                )->count(2)
+            )->count(2)
+        )->create()->first();
+        return $region;
+    }
+
 }
