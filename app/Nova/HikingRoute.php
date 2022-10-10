0e955b94b3732dfb08b692dbcb617718cfa2cf1d
marco <m.baroncini@cyberway.cloud>
2022-10-10 14:08:57 +0200

    Fix legenda field on HikingRoute resource

--- app/Nova/HikingRoute.php
+++ app/Nova/HikingRoute.php
@@ -157,7 +157,7 @@ class HikingRoute extends Resource
                 ->center($this->getCentroid()[1], $this->getCentroid()[0])
                 ->zoom(12)
                 ->hideFromIndex(),
-            Text::make('Legenda', function(){ return "<ul><li>Linea blu: percorso OSM2CAI/OSM</li><li>Linea rossa: percorso caricato dall'utente</li></ul>"; })->asHtml(),
+            Text::make('Legenda', function(){ return "<ul><li>Linea blu: percorso OSM2CAI/OSM</li><li>Linea rossa: percorso caricato dall'utente</li></ul>"; })->asHtml()->onlyOnDetail(),
             (new Tabs('Metadata', [
                 'Main' => $this->getMetaFields('main'),
                 'General' => $this->getMetaFields('general'),
