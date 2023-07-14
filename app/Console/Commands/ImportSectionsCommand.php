0e605fcdc5625eaaac01c23989069b7f7900611d
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-07-14 10:27:15 +0200

    clean comments on import sections command

--- app/Console/Commands/ImportSectionsCommand.php
+++ app/Console/Commands/ImportSectionsCommand.php
@@ -45,29 +45,15 @@ class ImportSectionsCommand extends Command
         $sectionFilePath = storage_path('imports/Sezioni.xlsx');
         $subsectionFilePath = storage_path('imports/SottoSezioni.xlsx');
 
-        // Importa le sezioni
+        // Import sections
         $this->info('Importing sections...');
         Excel::import(new SectionsImport, $sectionFilePath);
         $this->info('Sections imported successfully.');
 
-        // Importa le sottosezioni
+        // Import subsections
         $this->info('Importing subsections...');
         Excel::import(new SubSectionsImport, $subsectionFilePath);
         $this->info('Subsections imported successfully.');
-
-        // Associa le sezioni alla regione basandosi sul nome
-        // $this->info('Associating sections with regions...');
-        // $sections = Section::all();
-
-        // foreach ($sections as $section) {
-        //     $regionName = $section->region_name;
-        //     $region = Region::where('name', $regionName)->first();
-
-        //     if ($region) {
-        //         $section->region()->associate($region);
-        //         $section->save();
-        //     }
-        // }
 
         $this->info('Sections associated with regions successfully.');
     }
