26a886af28c8ed06e33a9aac5e641b84f9b4fb2e
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-07-14 10:05:40 +0200

    implemented sections_details_add_info 1282

--- app/Nova/Section.php
+++ app/Nova/Section.php
@@ -2,7 +2,7 @@
 
 namespace App\Nova;
 
-
+use App\Enums\IssueStatus;
 use App\Nova\HikingRoute;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
@@ -64,6 +64,20 @@ class Section extends Resource
     {
         $hikingRoutes = $this->hikingRoutes()->get();
 
+        //define the hiking routes for each osm2cai status
+        $hikingRoutesSDA1 = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->osm2cai_status == 1);
+        $hikingRoutesSDA2 = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->osm2cai_status == 2);
+        $hikingRoutesSDA3 = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->osm2cai_status == 3);
+        $hikingRoutesSDA4 = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->osm2cai_status == 4);
+
+        //define the hikingroutes for each issue status
+        $hikingRoutesSPS = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->issues_status == IssueStatus::Unknown);
+        $hikingRoutesSPP = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->issues_status == IssueStatus::Open);
+        $hikingRouteSPPP = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->issues_status == IssueStatus::PartiallyClosed);
+        $hikingRoutesSPNP = $hikingRoutes->filter(fn ($hikingRoute) => $hikingRoute->issues_status == IssueStatus::Closed);
+
+
+
         //create a string with the list of all the hiking routes and make it linkable to the hiking route resource detail
         $hikingRoutesString = '';
         foreach ($hikingRoutes as $hikingRoute) {
@@ -76,9 +90,15 @@ class Section extends Resource
         return [
             ID::make()->sortable()
                 ->hideFromIndex(),
-            Text::make('Nome', 'name')
+            Text::make('Nome', 'name',)
                 ->sortable()
-                ->rules('required', 'max:255'),
+                ->rules('required', 'max:255')
+                ->displayUsing(function ($name, $a, $b) {
+                    $wrappedName = wordwrap($name, 50, "\n", true);
+                    $htmlName = str_replace("\n", '<br>', $wrappedName);
+                    return $htmlName;
+                })
+                ->asHtml(),
             Text::make('Codice CAI', 'cai_code')
                 ->sortable()
                 ->rules('required', 'max:255'),
@@ -88,7 +108,46 @@ class Section extends Resource
             Text::make('Sentieri', function () use ($hikingRoutesString) {
                 return $hikingRoutesString;
             })->asHtml(),
-            HasMany::make('Utenti', 'users', User::class)
+            HasMany::make('Utenti', 'users', User::class),
+            Text::make('SDA1', function () use ($hikingRoutesSDA1) {
+                return $hikingRoutesSDA1->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SDA2', function () use ($hikingRoutesSDA2) {
+                return $hikingRoutesSDA2->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SDA3', function () use ($hikingRoutesSDA3) {
+                return $hikingRoutesSDA3->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SDA4', function () use ($hikingRoutesSDA4) {
+                return $hikingRoutesSDA4->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('TOT', function () use ($hikingRoutes) {
+                return $hikingRoutes->sum(function ($hikingRoute) {
+                    return ($hikingRoute->osm2cai_status < 5 && $hikingRoute->osm2cai_status > 0) ? 1 : 0;
+                });
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SPS', function () use ($hikingRoutesSPS) {
+                return $hikingRoutesSPS->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SPP', function () use ($hikingRoutesSPP) {
+                return $hikingRoutesSPP->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SPPP', function () use ($hikingRouteSPPP) {
+                return $hikingRouteSPPP->count();
+            })->onlyOnIndex()
+            ->sortable(),
+            Text::make('SPNP', function () use ($hikingRoutesSPNP) {
+                return $hikingRoutesSPNP->count();
+            })->onlyOnIndex()
+            ->sortable(),
+
 
         ];
     }
