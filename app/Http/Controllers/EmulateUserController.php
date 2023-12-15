a73cca13435b89e60cc9bd986aaba444bb86d88b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-12-15 14:08:54 +0100

    fix: fixed restore user when emulate

--- app/Http/Controllers/EmulateUserController.php
+++ app/Http/Controllers/EmulateUserController.php
@@ -9,6 +9,6 @@ class EmulateUserController extends Controller
     public function restore()
     {
         session(['emulate_user_id' => null]);
-        return redirect()->route('home');
+        return redirect('/');
     }
 }
