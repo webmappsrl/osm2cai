0d0d51cacc4dbce52888c9b71f9e690ffc6c113d
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-12-21 08:26:55 +0100

    feat: added ugc relationships

--- app/Models/UgcMedia.php
+++ app/Models/UgcMedia.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class UgcMedia extends Model
 {
     use HasFactory;
 
     protected $fillable = ['geohub_id', 'name', 'description', 'geometry', 'user_id', 'updated_at', 'raw_data', 'taxonomy_wheres', 'relative_url'];
+
+    public function ugc_pois(): BelongsToMany
+    {
+        return $this->belongsToMany(UgcPoi::class);
+    }
+
+    public function ugc_tracks(): BelongsToMany
+    {
+        return $this->belongsToMany(UgcTrack::class);
+    }
 }
