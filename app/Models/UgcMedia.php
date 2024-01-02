f961ff689165e6f4b6cbbf4deccf86e8e1e911ea
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-02 09:58:58 +0100

    fix: error fixed in ugcmedia

--- app/Models/UgcMedia.php
+++ app/Models/UgcMedia.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\GeojsonableTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
