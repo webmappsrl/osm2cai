5c8a1ee41e506102b8196fa70dd257058d286290
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-29 11:58:45 +0100

    feat: added osm_type to ecPoi table and enhanced nova to show type and osm url

--- app/Models/EcPoi.php
+++ app/Models/EcPoi.php
@@ -10,7 +10,7 @@ class EcPoi extends Model
 {
     use HasFactory, GeojsonableTrait;
 
-    protected $fillable = ['name', 'description', 'geometry', 'user_id', 'tags', 'type', 'osm_id'];
+    protected $fillable = ['name', 'description', 'geometry', 'user_id', 'tags', 'type', 'osm_id', 'osm_type'];
 
     public function user()
     {
