503d192ff5b5ba5b10ca925996cea1b8d695e639
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-31 09:42:46 +0100

    feat: added swagger docs for ecpois api

--- app/Http/Controllers/EcPoiController.php
+++ app/Http/Controllers/EcPoiController.php
@@ -9,7 +9,40 @@ use Illuminate\Http\Request;
 class EcPoiController extends Controller
 {
 
-
+    /**
+     * @OA\Get(
+     *     path="/ecpois/bb/{bounding_box}/{type}",
+     *     tags={"API V2"},
+     *     summary="Get Ec POIs by Bounding Box and Type",
+     *     description="Returns a list of Eco POIs within the specified bounding box and of the specified type",
+     *     @OA\Parameter(
+     *         name="bounding_box",
+     *         in="path",
+     *         required=true,
+     *         description="Bounding box in 'minLng,minLat,maxLng,maxLat' format",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="type",
+     *         in="path",
+     *         required=true,
+     *         description="Type of the POIs to retrieve: R for relation, W for way, N for node",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\AdditionalProperties(
+     *                 type="string",
+     *                 format="date-time"
+     *             )
+     *      
+     *         )
+     *     )
+     * )
+     */
     public function ecPoisBBox($bounding_box, $type)
     {
         // $bounding_box should be in 'minLng,minLat,maxLng,maxLat' format
@@ -31,6 +64,39 @@ class EcPoiController extends Controller
         return response()->json($pois);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/ecpois/{hr_osm2cai_id}/{type}",
+     *     tags={"Eco Points of Interest"},
+     *     summary="Get Eco POIs by OSM2CAI ID and Type",
+     *     description="Returns a list of Eco POIs associated with a specific OSM2CAI hiking route ID and of a specified type",
+     *     @OA\Parameter(
+     *         name="hr_osm2cai_id",
+     *         in="path",
+     *         required=true,
+     *         description="OSM2CAI ID of the hiking route",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Parameter(
+     *         name="type",
+     *         in="path",
+     *         required=true,
+     *         description="Type of the POIs to retrieve",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\AdditionalProperties(
+     *                 type="string",
+     *                 format="date-time"
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function ecPoisByOsm2CaiId($hr_osm2cai_id, $type)
     {
         $route = HikingRoute::where('id', $hr_osm2cai_id)->firstOrFail();
@@ -47,6 +113,39 @@ class EcPoiController extends Controller
         return response()->json($pois);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/ecpois/{hr_osm_id}/{type}",
+     *     tags={"Eco Points of Interest"},
+     *     summary="Get Eco POIs by OSM ID and Type",
+     *     description="Returns a list of Eco POIs associated with a specific OpenStreetMap hiking route ID and of a specified type",
+     *     @OA\Parameter(
+     *         name="hr_osm_id",
+     *         in="path",
+     *         required=true,
+     *         description="OSM ID of the hiking route",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Parameter(
+     *         name="type",
+     *         in="path",
+     *         required=true,
+     *         description="Type of the POIs to retrieve",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\AdditionalProperties(
+     *                 type="string",
+     *                 format="date-time"
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function ecPoisByOsmId($hr_osm_id, $type)
     {
         $route = HikingRoute::where('relation_id', $hr_osm_id)->firstOrFail();
