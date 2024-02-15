635fc4ef10b921bb9cdc01b21a54e46385557187
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-02-15 19:25:31 +0100

    test2

--- scripts/db_dump_restore.sh
+++ scripts/db_dump_restore.sh
@@ -3,16 +3,26 @@
 # Variabili
 SERVER_PROD="root@116.203.180.132"
 DUMP_REMOTE_PATH="/root/osm2cai/storage/app/database/osm2cai/last-dump.sql.gz"
-DUMP_LOCAL_PATH="/root/html/osm2cai/storage/app/databaselast-dump.sql.gz"
+DUMP_LOCAL_PATH="/root/html/osm2cai/storage/app/database/last-dump.sql.gz" # Corretto il percorso
 DB_NAME="osm2cai"
 DB_USER="postgres"
 DB_PASSWORD="9PDev#2sB&zDzIzL47yar4s"
 
+# Scarica il dump dal server di produzione
 scp $SERVER_PROD:$DUMP_REMOTE_PATH $DUMP_LOCAL_PATH
 
+# Decomprimi il dump
 gzip -d $DUMP_LOCAL_PATH
 
-export PGPASSWORD=$DB_PASSWORD
+
+# Elimina il database esistente
+dropdb -h localhost -U $DB_USER --if-exists $DB_NAME
+
+# Crea un nuovo database vuoto
+createdb -h localhost -U $DB_USER $DB_NAME
+
+# Importa il dump nel nuovo database
 psql -h localhost -U $DB_USER -d $DB_NAME -f "${DUMP_LOCAL_PATH%.gz}"
 
+# Pulizia
 rm "${DUMP_LOCAL_PATH%.gz}"
