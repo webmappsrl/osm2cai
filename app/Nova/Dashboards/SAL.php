46016d7236e548cc1989e7d40a850024dbc689ef
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-02-15 13:08:10 +0100

    feat: updated mitur abruzzo page and sync tool in nova

--- app/Nova/Dashboards/SAL.php
+++ app/Nova/Dashboards/SAL.php
@@ -27,13 +27,16 @@ class SAL extends Dashboard
                 DB::raw('(SELECT COUNT(DISTINCT mountain_group_id) FROM mountain_groups_region WHERE region_id = regions.id) as mountain_groups_count'),
                 DB::raw('COUNT(DISTINCT ec_pois.id) as ec_pois_count'),
                 DB::raw('(SELECT COUNT(DISTINCT hr.id) FROM (SELECT hiking_route_id as id FROM hiking_route_region WHERE region_id = regions.id) as hr JOIN hiking_routes ON hr.id = hiking_routes.id WHERE hiking_routes.osm2cai_status = 4) as hiking_routes_count'),
-                DB::raw('(COUNT(DISTINCT ec_pois.id) + (SELECT COUNT(DISTINCT hr.id) FROM (SELECT hiking_route_id as id FROM hiking_route_region WHERE region_id = regions.id) as hr JOIN hiking_routes ON hr.id = hiking_routes.id  WHERE hiking_routes.osm2cai_status = 4)) as poi_total'),
+                DB::raw('(COUNT(DISTINCT ec_pois.id) + (SELECT COUNT(DISTINCT hr.id) FROM (SELECT hiking_route_id as id FROM hiking_route_region WHERE region_id = regions.id) as hr JOIN hiking_routes ON hr.id = hiking_routes.id WHERE hiking_routes.osm2cai_status = 4)) as poi_total'),
                 DB::raw('COUNT(DISTINCT sections.id) as sections_count'),
+                DB::raw('COUNT(DISTINCT cai_huts.id) as cai_huts_count'), // Aggiungi questa riga
             ])
             ->leftJoin('ec_pois', 'regions.id', '=', 'ec_pois.region_id')
             ->leftJoin('sections', 'regions.id', '=', 'sections.region_id')
+            ->leftJoin('cai_huts', 'regions.id', '=', 'cai_huts.region_id') // Aggiungi questa join
             ->groupBy('regions.id')
             ->get();
+
 
         // Create the HTML string
         $html = '<table class="table-auto w-full mt-5 ">
@@ -55,6 +58,7 @@ class SAL extends Dashboard
         $sumHikingRoutes = 0;
         $sumPoiTotal = 0;
         $sumSections = 0;
+        $sumCaiHuts = 0;
 
         foreach ($regions as $region) {
             $sumMountainGroups += $region->mountain_groups_count;
@@ -62,12 +66,13 @@ class SAL extends Dashboard
             $sumHikingRoutes += $region->hiking_routes_count;
             $sumPoiTotal += $region->poi_total;
             $sumSections += $region->sections_count;
+            $sumCaiHuts += $region->cai_huts_count;
 
             $html .= "<tr class='border-b'>
         <td class='px-4 py-2'>{$region->name}</td>
         <td class='px-4 py-2'>{$region->mountain_groups_count}</td>
         <td class='px-4 py-2'>{$region->ec_pois_count}</td>
-        <td class='px-4 py-2'></td>
+        <td class='px-4 py-2'> {$region->cai_huts_count}</td>
         <td class='px-4 py-2'>{$region->hiking_routes_count}</td>
         <td class='px-4 py-2'>{$region->poi_total}</td>
         <td class='px-4 py-2'>{$region->sections_count}</td>
