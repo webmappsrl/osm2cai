c519210652181316f3d6964837a74f3de9b7805d
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-02-15 19:14:18 +0100

    added dump restore script

---
+++ scripts/db_dump_restore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variabili
+SERVER_PROD="root@116.203.180.132"
+DUMP_REMOTE_PATH="/root/osm2cai/storage/app/database/osm2cai/last-dump.sql.gz"
+DUMP_LOCAL_PATH="~/storage/app/database/last-dump.sql.gz"
+DB_NAME="osm2cai"
+DB_USER="postgres"
+DB_PASSWORD="9PDev#2sB&zDzIzL47yar4s"
+
+scp $SERVER_PROD:$DUMP_REMOTE_PATH $DUMP_LOCAL_PATH
+
+gzip -d $DUMP_LOCAL_PATH
+
+export PGPASSWORD=$DB_PASSWORD
+psql -h localhost -U $DB_USER -d $DB_NAME -f "${DUMP_LOCAL_PATH%.gz}"
+
+rm "${DUMP_LOCAL_PATH%.gz}"
