794176a07c0fdd3a33661e2793af75ea14124d96
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-02-23 15:28:51 +0100

    feat: added filters to ecpois

---
+++ app/Nova/Filters/EcPoiTypeFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use App\Enums\EcPoiTypes;
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class EcPoiTypeFilter extends Filter
+{
+    public $name = 'Tipo';
+
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('type', $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        $options = EcPoiTypes::cases();
+        return $options;
+    }
+}
