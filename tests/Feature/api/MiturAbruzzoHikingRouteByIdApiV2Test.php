b6953c0e888ae25adb1a170fa4d885a56cfb75c7
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-04 09:48:24 +0100

    feat: tests for mitur abruzzo api

---
+++ tests/Feature/api/MiturAbruzzoHikingRouteByIdApiV2Test.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Feature\api;
+
+use Tests\TestCase;
+use App\Models\User;
+use GeoJson\Geometry\LineString;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class MiturAbruzzoHikingRouteByIdApiV2Test extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $user;
+    protected $hr;
+
+    protected function setUp(): void
+    {
+
+        parent::setUp();
+        $line = new LineString([[0, 0], [1, 1]]);
+        $res = DB::select(DB::raw('SELECT ST_GeomFromGeoJSON(\'' . json_encode($line->jsonSerialize()) . '\') as geom'));
+        $this->user = User::where('email', 'team@webmapp.it')->first();
+        $this->hr = \App\Models\HikingRoute::create([
+            'id' => 12,
+            'relation_id' => 123445553,
+            'osm2cai_status' => 4,
+            'deleted_on_osm' => false,
+            'region_favorite' => true,
+            'issues_status' => 'sconosciuto',
+            'geometry' => $res[0]->geom
+        ]);
+    }
+
+    /**
+     * @test
+     * 
+     * It returns a 200 status code
+     * 
+     * @return void
+     */
+    public function test_it_returns_a_200_status_code()
+    {
+        $response = $this->actingAs($this->user, 'api')
+            ->get('/api/v2/mitur_abruzzo/hiking_route/' . $this->hr->id);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     * 
+     * It returns a 404 status code if the hikingroute is not found
+     * 
+     * @return void
+     */
+    public function test_it_returns_a_404_status_code_if_the_hikingroute_is_not_found()
+    {
+        $response = $this->actingAs($this->user, 'api')
+            ->get('/api/v2/mitur_abruzzo/hiking_route/999999');
+        $response->assertStatus(404);
+    }
+
+    /**
+     * @test
+     * 
+     * It returns a correct json structure
+     * 
+     * @return void
+     */
+    public function test_it_creates_the_correct_json_structure()
+    {
+        $response = $this->actingAs($this->user, 'api')
+            ->get('/api/v2/mitur_abruzzo/hiking_route/' . $this->hr->id);
+        $response->assertJsonStructure([
+            'type',
+            'properties',
+            'properties' => [
+                'id',
+                'ref',
+            ],
+            'geometry',
+        ]);
+    }
+}
