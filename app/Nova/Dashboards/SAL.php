1e38dcec5f6c88d532a989ef886c32360048b7da
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-06 11:05:45 +0100

    Feat: improved loading speed for dashboard mitur-abruzzo

--- app/Nova/Dashboards/SAL.php
+++ app/Nova/Dashboards/SAL.php
@@ -21,22 +21,7 @@ class SAL extends Dashboard
      */
     public function cards()
     {
-        $regions = DB::table('regions')
-            ->select([
-                'regions.name',
-                DB::raw('(SELECT COUNT(DISTINCT mountain_group_id) FROM mountain_groups_region WHERE region_id = regions.id) as mountain_groups_count'),
-                DB::raw('COUNT(DISTINCT ec_pois.id) as ec_pois_count'),
-                DB::raw('(SELECT COUNT(DISTINCT hr.id) FROM (SELECT hiking_route_id as id FROM hiking_route_region WHERE region_id = regions.id) as hr JOIN hiking_routes ON hr.id = hiking_routes.id WHERE hiking_routes.osm2cai_status = 4) as hiking_routes_count'),
-                DB::raw('(COUNT(DISTINCT ec_pois.id) + (SELECT COUNT(DISTINCT hr.id) FROM (SELECT hiking_route_id as id FROM hiking_route_region WHERE region_id = regions.id) as hr JOIN hiking_routes ON hr.id = hiking_routes.id WHERE hiking_routes.osm2cai_status = 4)) as poi_total'),
-                DB::raw('COUNT(DISTINCT sections.id) as sections_count'),
-                DB::raw('COUNT(DISTINCT cai_huts.id) as cai_huts_count'), // Aggiungi questa riga
-            ])
-            ->leftJoin('ec_pois', 'regions.id', '=', 'ec_pois.region_id')
-            ->leftJoin('sections', 'regions.id', '=', 'sections.region_id')
-            ->leftJoin('cai_huts', 'regions.id', '=', 'cai_huts.region_id') // Aggiungi questa join
-            ->groupBy('regions.id')
-            ->get();
-
+        $regions = DB::table('regions')->get();
 
         // Create the HTML string
         $html = '<table class="table-auto w-full mt-5 ">
@@ -61,22 +46,24 @@ class SAL extends Dashboard
         $sumCaiHuts = 0;
 
         foreach ($regions as $region) {
-            $sumMountainGroups += $region->mountain_groups_count;
-            $sumEcPois += $region->ec_pois_count;
-            $sumHikingRoutes += $region->hiking_routes_count;
-            $sumPoiTotal += $region->poi_total;
-            $sumSections += $region->sections_count;
-            $sumCaiHuts += $region->cai_huts_count;
+            $data = json_decode($region->aggregated_data);
 
             $html .= "<tr class='border-b'>
         <td class='px-4 py-2'>{$region->name}</td>
-        <td class='px-4 py-2'>{$region->mountain_groups_count}</td>
-        <td class='px-4 py-2'>{$region->ec_pois_count}</td>
-        <td class='px-4 py-2'> {$region->cai_huts_count}</td>
-        <td class='px-4 py-2'>{$region->hiking_routes_count}</td>
-        <td class='px-4 py-2'>{$region->poi_total}</td>
-        <td class='px-4 py-2'>{$region->sections_count}</td>
+        <td class='px-4 py-2'>{$data->mountain_groups_count}</td>
+        <td class='px-4 py-2'>{$data->ec_pois_count}</td>
+        <td class='px-4 py-2'>{$data->cai_huts_count}</td>
+        <td class='px-4 py-2'>{$data->hiking_routes_count}</td>
+        <td class='px-4 py-2'>{$data->poi_total}</td>
+        <td class='px-4 py-2'>{$data->sections_count}</td>
     </tr>";
+
+            $sumMountainGroups += $data->mountain_groups_count;
+            $sumEcPois += $data->ec_pois_count;
+            $sumHikingRoutes += $data->hiking_routes_count;
+            $sumPoiTotal += $data->poi_total;
+            $sumSections += $data->sections_count;
+            $sumCaiHuts += $data->cai_huts_count;
         }
 
         $html .= "<tr class='border-t'>
