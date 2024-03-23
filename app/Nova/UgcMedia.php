e44cc532c1940cea5a30a9b9a63e93a97872fef7
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-03-23 08:57:16 +0100

    feat: added media thumbnail field in ugc media nova resource

--- app/Nova/UgcMedia.php
+++ app/Nova/UgcMedia.php
@@ -85,6 +85,9 @@ class UgcMedia extends Resource
             BelongsTo::make('User', 'user')
                 ->searchable()
                 ->sortable(),
+            Text::make('Media', function () {
+                return "<a href='{$this->relative_url}' target='_blank'><img src='{$this->relative_url}' style='max-width: 100px; max-height: 100px; border: 1px solid #ccc; border-radius: 10%; padding: 2px;' alt='Thumbnail'></a>";
+            })->asHtml(),
             BelongsToMany::make('UGC Pois', 'ugc_pois', UgcMedia::class),
             BelongsToMany::make('UGC Tracks', 'ugc_tracks', UgcMedia::class),
             Text::make('Tassonomie Where', 'taxonomy_wheres')
