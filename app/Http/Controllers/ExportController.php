6bf174072ebb1bf64b8f2e7951281372b9376d17
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-04-18 10:29:39 +0200

    feat: implemented users export api

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\HikingRoute;
 use Illuminate\Http\Request;
 use App\Http\Resources\HikingRouteResource;
 use App\Http\Resources\HikingRouteResourceCollection;
+use App\Http\Resources\UserResource;
 
 class ExportController extends Controller
 {
@@ -25,5 +27,19 @@ class ExportController extends Controller
     {
         $hikingRoute = HikingRoute::find($id);
         return new HikingRouteResource($hikingRoute);
+    }
+
+    public function UsersList()
+    {
+
+        return response()->json(User::all('id', 'updated_at')->mapWithKeys(function ($user) {
+
+            return [$user->id => $user->updated_at];
+        }));
+    }
+
+    public function UsersSingleFeature($id)
+    {
+        return new UserResource(User::find($id));
     }
 }
