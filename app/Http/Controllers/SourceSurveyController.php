c7643acec73f321b1c07b14eef3545564a8ea4f5
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-06-27 08:57:21 +0200

    fix: fixed acquasorgente overlays api

--- app/Http/Controllers/SourceSurveyController.php
+++ app/Http/Controllers/SourceSurveyController.php
@@ -25,14 +25,41 @@ class SourceSurveyController extends Controller
             if ($date !== 'N/A') {
                 $date = Carbon::parse($date)->format('d-m-Y');
             }
-            $flowRate = str_replace(',', '.', $sourceSurvey->flow_rate);
-            $conductivity = str_replace(',', '.', $sourceSurvey->conductivity) . ' microS/cm';
+            if (isset($sourceSurvey->flow_rate) && $sourceSurvey->flow_rate !== 'N/A') {
+                $flowRate = str_replace(',', '.', $sourceSurvey->flow_rate) . ' L/s';
+            } else {
+                $flowRate = 'N/A';
+            }
+            if (isset($sourceSurvey->temperature) && $sourceSurvey->temperature !== 'N/A') {
+                $temperature = str_replace(',', '.', $sourceSurvey->temperature) . 'C';
+            } else {
+                $temperature = 'N/A';
+            }
+            if (isset($sourceSurvey->conductivity) && $sourceSurvey->conductivity !== 'N/A') {
+                $conductivity = str_replace(',', '.', $sourceSurvey->conductivity) . ' microS/cm';
+            } else {
+                $conductivity = 'N/A';
+            }
+            switch ($sourceSurvey->is_water_source_active) {
+                case true:
+                    $isActive = 'SI';
+                    break;
+
+                case false:
+                    $isActive = 'NO';
+                    break;
+
+                default:
+                    $isActive = 'N/A';
+                    break;
+            }
 
             $htmlString = <<<HTML
     <div style='font-size: 1.1em; line-height: 1.4em;'>
         <strong>Data del monitoraggio:</strong> <span style='white-space: pre-wrap;'>$date,</span><br>
+        <strong>Sorgente Attiva:</strong> <span style='white-space: pre-wrap;'>$isActive,</span><br>
         <strong>Portata:</strong> <span style='white-space: pre-wrap;'>$flowRate,</span><br>
-        <strong>Temperatura:</strong> <span style='white-space: pre-wrap;'>$sourceSurvey->temperature,</span><br>
+        <strong>Temperatura:</strong> <span style='white-space: pre-wrap;'>$temperature,</span><br>
         <strong>Conducibilitá elettrica:</strong> <span style='white-space: pre-wrap;'>$conductivity,</span><br>
     </div>
     HTML;
