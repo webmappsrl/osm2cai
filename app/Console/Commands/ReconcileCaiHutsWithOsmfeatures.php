2fa0b720ae43b52152ba0572f385983c836502b6
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-07-16 15:22:38 +0200

    chore: refactoring

--- app/Console/Commands/ReconcileCaiHutsWithOsmfeatures.php
+++ app/Console/Commands/ReconcileCaiHutsWithOsmfeatures.php
@@ -9,118 +9,202 @@ use Illuminate\Support\Facades\Log;
 
 class ReconcileCaiHutsWithOsmfeatures extends Command
 {
-    protected $signature = 'osm2cai:reconcile-cai_huts {id?}';
-    protected $description = 'This command reconciles cai_huts with OsmFeatures API based on cai hut geometry using the Osmfeatures api /api/v1/features/places/{lon}/{lat}/{distance}. It saves osmfeatures_id in cai_huts table fo the nearest place found.';
+    /**
+     * @var string
+     */
+    protected $signature = 'osm2cai:reconcile-cai_huts {id? : CaiHut id}';
 
+    /**
+     * @var string
+     */
+    protected $description = 'This command reconciles cai_huts with OsmFeatures API based on cai hut geometry using the Osmfeatures api /api/v1/features/places/{lon}/{lat}/{distance}. It saves osmfeatures_id in cai_huts table for the nearest place found.';
+
+    /**
+     * @var \Illuminate\Log\Logger
+     */
     protected $logger;
 
+    /**
+     * @var array
+     */
+    protected $notReconciledHuts = [];
 
     public function __construct()
     {
         $this->logger = Log::channel('caiHutsReconciliations');
         parent::__construct();
     }
-    public function handle()
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
     {
-
         $id = $this->argument('id');
-        $all = $this->argument('id') ? false : true;
-
-        if ($all) {
-            $huts = DB::table('cai_huts')->get();
-        } else {
-            $huts = DB::table('cai_huts')->where('id', $id)->get();
-        }
+        $huts = $this->getHuts($id);
 
         foreach ($huts as $hut) {
             $this->reconcileHut($hut);
         }
 
+        $this->logReconciliationSummary();
         $this->info('Reconciliation completed.');
-        $this->logger->info('Reconciliation completed.');
         return 0;
     }
 
-    private function reconcileHut($hut, int $distance = 1000)
+    /**
+     * @param int|null $id
+     * @return \Illuminate\Support\Collection
+     */
+    private function getHuts(?int $id = null): \Illuminate\Support\Collection
     {
-        $this->logger->info('Reconciling hut ' . $hut->id);
-        $this->info('Reconciling hut ' . $hut->id);
-        //get coordinates for the hut
+        return $id ? DB::table('cai_huts')->where('id', $id)->get() : DB::table('cai_huts')->get();
+    }
+
+    /**
+     * @param \stdClass $hut
+     * @param int $distance
+     * @return void
+     */
+    private function reconcileHut(\stdClass $hut, int $distance = 1000): void
+    {
+        $this->logInfo('Reconciling hut ' . $hut->id);
+
+        $coordinates = $this->getCoordinates($hut);
+        if (!$coordinates) {
+            $this->logError($hut, 'Unable to fetch coordinates.');
+            return;
+        }
+
+        $response = $this->fetchOsmFeatures($coordinates, $distance);
+        if ($response === null || $response->failed()) {
+            $this->logError($hut, 'API request failed.');
+            return;
+        }
+
+        $places = $response->json();
+        if (empty($places)) {
+            $this->logWarning($hut, 'API response is empty. Increasing distance.');
+            $this->reconcileHut($hut, $distance * 2);
+            return;
+        }
+
+        $matchingPlace = $this->getMatchingPlace($places);
+        if ($matchingPlace) {
+            $this->updateHut($hut, $matchingPlace);
+        } else {
+            $this->logError($hut, 'No matching place found.');
+        }
+    }
+
+    /**
+     * @param \stdClass $hut
+     * @return \stdClass|null
+     */
+    private function getCoordinates(\stdClass $hut): ?\stdClass
+    {
         try {
-            $coordinates = DB::table('cai_huts')
+            return DB::table('cai_huts')
                 ->select(DB::raw('ST_X(geometry::geometry) AS longitude, ST_Y(geometry::geometry) AS latitude'))
                 ->where('id', $hut->id)
                 ->first();
         } catch (\Exception $e) {
             $this->logger->warning($e->getMessage());
-            $this->info('Failed to fetch coordinates for hut ' . $hut->id);
-            return;
+            return null;
         }
+    }
 
-        if (!$coordinates) {
-            $this->logger->warning('No coordinates found for hut ' . $hut->id);
-            $this->info('No coordinates found for hut ' . $hut->id);
-            return;
-        }
-
-        //http request to osmfeatures api
+    /**
+     * @param \stdClass $coordinates
+     * @param int $distance
+     * @return \Illuminate\Http\Client\Response|null
+     */
+    private function fetchOsmFeatures(\stdClass $coordinates, int $distance): ?\Illuminate\Http\Client\Response
+    {
         try {
-            $response = Http::get('https://osmfeatures.maphub.it/api/v1/features/places/' . $coordinates->longitude . '/' . $coordinates->latitude . '/' . $distance);
+            return Http::get("https://osmfeatures.maphub.it/api/v1/features/places/{$coordinates->longitude}/{$coordinates->latitude}/{$distance}");
         } catch (\Exception $e) {
-            $this->logger->warning('API request failed for hut ' . $hut->name . ' with id ' . $hut->id . '. Error: ' . $e->getMessage());
-            $this->info('API request failed for hut ' . $hut->name . ' with id ' . $hut->id . '. Error: ' . $e->getMessage());
-            return;
+            $this->logger->warning('API request failed. Error: ' . $e->getMessage());
+            return null;
         }
+    }
 
-        if ($response->failed()) {
-            $this->logger->warning('API request failed for hut ' . $hut->name . ' with id ' . $hut->id);
-            $this->info('API request failed for hut ' . $hut->name . ' with id ' . $hut->id);
-            return;
-        }
-
-        //if the response is empty try to increase distance
-        if ($response->json() === []) {
-            $this->logger->warning('API response is empty for hut ' . $hut->name . ' with id ' . $hut->id . '. increasing distance');
-            $this->info('API response is empty for hut ' . $hut->name . ' with id ' . $hut->id . '. increasing distance');
-            $distance = $distance * 2;
-            $this->reconcileHut($hut, $distance);
-            return;
-        }
-
-        $places = $response->json();
-
+    /**
+     * @param array $places
+     * @return array|null
+     */
+    private function getMatchingPlace(array $places): ?array
+    {
         $matchingPlaces = [];
-
-        //filter by class and subclass
-        $this->logger->info('filtering places by class (tourism/amenity) and subclass(huts/shelter)');
-        $this->info('filtering places by class (tourism/amenity) and subclass(huts/shelter)');
         foreach ($places as $place) {
-            if (
-                ($place['class'] === 'tourism' && $place['subclass'] === 'huts') ||
+            if (($place['class'] === 'tourism' && $place['subclass'] === 'huts') ||
                 ($place['class'] === 'amenity' && $place['subclass'] === 'shelter')
             ) {
                 $matchingPlaces[] = $place;
             }
         }
-        $this->logger->info('Found ' . count($matchingPlaces) . ' matching places for hut ' . $hut->id);
-        $this->info('Found ' . count($matchingPlaces) . ' matching places for hut ' . $hut->id);
+        //osmfeatures api is ordered by distance so the first result is the closest
+        return $matchingPlaces[0] ?? null;
+    }
 
-        //get the nearest place
-        if (count($matchingPlaces) > 0) {
-            $this->logger->info('Getting nearest place...');
-            $this->info('Getting nearest place...');
-            //OSMFEATURES API (EXAMPLE :https://osmfeatures.maphub.it/api/v1/features/places/10.494953/46.179482/1000) already ordered by distance so the first result is the nearest
-            $matchingPlace = $matchingPlaces[0];
+    /**
+     * @param \stdClass $hut
+     * @param array $matchingPlace
+     * @return void
+     */
+    private function updateHut(\stdClass $hut, array $matchingPlace): void
+    {
+        DB::table('cai_huts')->where('id', $hut->id)->update([
+            'osmfeatures_id' => $matchingPlace['osmfeatures_id']
+        ]);
+        $this->logInfo('Updated hut ' . $hut->id . ' with OSMFeatures ID ' . $matchingPlace['osmfeatures_id']);
+    }
 
-            $this->logger->info('nearest place: https://osmfeatures.maphub.it/api/v1/features/places/' . $matchingPlace['osmfeatures_id']);
-            $this->info('nearest place: https://osmfeatures.maphub.it/api/v1/features/places/' . $matchingPlace['osmfeatures_id']);
-            //update the hut with the osmfeatures_id
-            if ($matchingPlace) {
-                DB::table('cai_huts')->where('id', $hut->id)->update([
-                    'osmfeatures_id' => $matchingPlace['osmfeatures_id']
-                ]);
-            } else {
-                $this->logger->warning('No matching place found for hut ' . $hut->id);
+    /**
+     * @param \stdClass $hut
+     * @param string $message
+     * @return void
+     */
+    private function logError(\stdClass $hut, string $message): void
+    {
+        $url = url('resource/cai-huts/' . $hut->id);
+        $this->logger->error("Reconciliation failed for CAI Hut ID {$hut->id}: {$message}");
+        $this->info("Reconciliation failed for CAI Hut ID {$hut->id}: {$message}");
+        $this->notReconciledHuts[] = ['url' => $url, 'reason' => $message];
+    }
+
+    /**
+     * @param \stdClass $hut
+     * @param string $message
+     * @return void
+     */
+    private function logWarning(\stdClass $hut, string $message): void
+    {
+        $this->logger->warning("Warning for CAI Hut ID {$hut->id}: {$message}");
+        $this->info("Warning for CAI Hut ID {$hut->id}: {$message}");
+    }
+
+    /**
+     * @param string $message
+     * @return void
+     */
+    private function logInfo(string $message): void
+    {
+        $this->logger->info($message);
+        $this->info($message);
+    }
+
+    /**
+     * @return void
+     */
+    private function logReconciliationSummary(): void
+    {
+        if (count($this->notReconciledHuts) > 0) {
+            $this->logInfo('Reconciliation summary:');
+            foreach ($this->notReconciledHuts as $hut) {
+                $this->logInfo("Hut URL: {$hut['url']} - Reason: {$hut['reason']}");
             }
         }
     }
