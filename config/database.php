3d2632a8f21d6ce93aa07c10064c9e079cc74ec9
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2024-10-01 08:36:18 +0200

    allineamento utenti geohub OC:4052 (#143)
    * feat: configured connection to geohub database
    
    * chore: geohub users list
    
    * feat: align users command

--- config/database.php
+++ config/database.php
@@ -93,6 +93,19 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'geohub' => [
+            'driver' => 'pgsql',
+            'host' => env('GEOHUB_DB_HOST'),
+            'port' => env('GEOHUB_DB_PORT', '5432'),
+            'database' => env('GEOHUB_DB_DATABASE'),
+            'username' => env('GEOHUB_DB_USERNAME'),
+            'password' => env('GEOHUB_DB_PASSWORD'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
